bf87067e2938b4ead49271bd6e76b8f87c35ee7e
Jared Stevick <jstevick@gmail.com>
2015-01-22 12:33:56 -0500

    added the start of the right way to place the chess pieces.  also added the beginnings of the string calculator

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,30 @@
 var assert = require('assert');
 
-/**
- * @param Number A
- * @param Number B
- * @return sum of A and B
- */
-function add(A, B){
-    return A + B;
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
+
+/*
+* @param String A representing an English number word
+* @param String B representing an English number word
+* @return Number
+*/
+
+function plus(A, B) {
+    if ( A == "zero" ){
+        return 0;
+    }
+    return 2;
 }
-console.log(add(4, 5));
 
-assert(add(1, 1), 3);
-assert(add(4, 7));
-
-
-/**
- * @param Number A
- * @param Number B
- * @return difference of A and B
- */
-function diff(A, B){
-    return A - B;
-}
-console.log(diff(4, 7));
-assert(diff(5, 2));
-
-
-
-/**
- * @param Number A
- * @param Number B
- * @return product of A times B
- */
-function prod(A, B){
-    return A * B;
-}
-console.log(prod(2,3));
-assert(prod(4, 5));
-assert(prod(11, 2));
-
-
-/**
- * @param Number A
- * @param Number B
- * @return division of A by B
- */
-function div(A, B){
-    return A / B;
-}
-console.log(div(12, 3));
-assert(div(10, 5));
-assert(div(6, 2));
-
-
-
-
-
-string calculator:
-
-//definitely need to define the numbers//
-
-'zero' = 0
-'one' = 1
-'two' = 2
-'three' = 3
-'four' = 4
-'five' = 5
-'six' = 6
-'seven' = 7
-'eight' = 8
-'nine' = 9
-'ten' = 10
-
-//not sure what to do here yet//
-function plus(string1, string2) {
-    return Number;
-}
+it('should add "number words"', function() {
+    assert.equal(plus("one", "one"), 2);
+});
+it('should add "zero" and "zero"', function() {
+    assert.equal(plus("zero", "zero"), 0);
+});
