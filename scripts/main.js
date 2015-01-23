866953f70f9268ec121a9123b98392c160019ffc
Jared Stevick <jstevick@gmail.com>
2015-01-22 21:40:10 -0500

    made the pieces move

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+<<<<<<< Updated upstream
+=======
+var board = [
+    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] ];
+
+console.log(board.join('\n') + '\n\n');
+
+// Move King's Pawn forward 2
+//board[4][4] = board[6][4];
+//board[6][4] = ' ';
+//console.log(board.join('\n'));
+
+// move rook's pawn forward one
+//board[2][0] = board[1][0];
+//board[1][0] = ' ';
+//console.log(board.join('\n'));
+// wooohoo it worked!
+
+// attempting to move the knight
+//board[5][0] = board[7][1];
+//board[7][1] = ' ';
+//console.log(board.join('\n'));
+// success!!
+>>>>>>> Stashed changes
