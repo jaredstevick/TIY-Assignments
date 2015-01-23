1bf02645628d21716869cbe06550022f6e1278a2
Jared Stevick <jstevick@gmail.com>
2015-01-23 12:13:50 -0500

    another ten with the string calculator

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -6,7 +6,6 @@ var assert = require('assert');
 //it('should add "zero" and "zero"', function(){
 //    assert.equal(plus("zero", "zero"), 0);
 //});
-
 //
 //function plus(A, B) {
 //    return (1);
@@ -14,7 +13,6 @@ var assert = require('assert');
 //it('should add "one and "zero"', function() {
 //    assert.equal(plus("one", "zero"), 1);
 //});
-
 //
 //function plus(A, B) {
 //    return (1);
@@ -82,9 +80,75 @@ var assert = require('assert');
 //it('should add "one" and "nine"', function() {
 //   assert.equal(plus("one", "nine"), 10);
 //});
-function plus(A, B) {
-    return (11);
+//function plus(A, B) {
+//    return (11);
+//}
+//it('should add "one" and "ten"', function() {
+//    assert.equal(plus("one", "ten"), 11);
+//});
+//function minus(A, B) {
+//    return (2)
+//}
+//it('should subtract "zero" from "two"', function() {
+//    assert.equal(minus("two", "zero"), 2);
+//});
+//function minus(A, B) {
+//    return (1);
+//}
+//it('should subtract "one" from "two"', function() {
+//    assert.equal(minus("two", "one"), 1);
+//});
+//function minus(A, B) {
+//    return (0);
+//}
+//it('should subtract "two" from "two"', function() {
+//    assert.equal(minus("two", "two"), 0);
+//});
+//function minus(A, B) {
+//    return (1);
+//}
+//it('should subtract "two" from "three"', function() {
+//    assert.equal(minus("three", "two"), 1);
+//});
+//function minus(A, B) {
+//    return (2);
+//}
+//it('should subtract "two" from "four"', function() {
+//    assert.equal(minus("four", "two"), 2);
+//});
+//function minus(A, B) {
+//    return (3);
+//}
+//it('should subtract "two" from "five"', function() {
+//    assert.equal(minus("five", "two"), 3);
+//});
+//function product(A, B) {
+//    return (12);
+//}
+//it('should return the product of "two" and "six"', function() {
+//    assert.equal(product("two", "six"), 12);
+//});
+//function product(A, B) {
+//    return (14);
+//}
+//it('should return the product of "two" and "seven"', function() {
+//    assert.equal(product("two", "seven"), 14);
+//});
+//function product(A, B) {
+//    return (16);
+//}
+//it('should return the product of "two" and "eight"', function() {
+//    assert.equal(product("two", "eight"), 16);
+//});
+//function product(A, B) {
+//    return (18);
+//}
+//it('should return the product of "two" and "nine"', function() {
+//    assert.equal(product("two", "nine"), 18);
+//});
+function product(A, B) {
+    return (20);
 }
-it('should add "one" and "ten"', function() {
-    assert.equal(plus("one", "ten"), 11);
+it('should return the product of "two" and "ten"', function() {
+    assert.equal(product("two", "ten"), 20);
 });
