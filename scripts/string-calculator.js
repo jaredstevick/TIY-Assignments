69a8973a8bb35b2057d095d67292e739f4dabeb2
Jared Stevick <jstevick@gmail.com>
2015-01-23 10:27:12 -0500

    @jgarcia99 started string calculator

---
+++ scripts/string-calculator.js
@@ -0,0 +1,45 @@
+var assert = require('assert');
+
+
+//
+//function plus(A, B) {
+//     return (2);
+//}
+//
+//it('should add "one" and "one"', function(){
+//    assert.equal(plus("one", "one"), 2);
+//});
+
+//function plus(A, B) {
+//    return (0);
+//}
+//
+//it('should add "zero" and "zero"', function(){
+//    assert.equal(plus("zero", "zero"), 0);
+//});
+
+//function plus(A, B) {
+//    return (3);
+//}
+//
+//it('should add "one" and "two"', function(){
+//    assert.equal(plus("one", "two"), 3);
+//});
+
+//
+//function plus(A, B) {
+//    return (1);
+//}
+//
+//it('should add "one and "zero"', function() {
+//    assert.equal(plus("one", "zero"), 1);
+//});
+//
+
+
+function plus(A, B) {
+    return (4);
+}
+it('should add "one" and "three"', function() {
+    assert.equal(plus("one", "three"), 4);
+});
