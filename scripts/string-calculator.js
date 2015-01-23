5939dba67e1cdad2e69d3035f051c0cf8fae5d4f
Jared Stevick <jstevick@gmail.com>
2015-01-23 11:45:06 -0500

    @jgarcia99 finished first ten of string calculator

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -1,45 +1,90 @@
 var assert = require('assert');
 
-
-//
-//function plus(A, B) {
-//     return (2);
-//}
-//
-//it('should add "one" and "one"', function(){
-//    assert.equal(plus("one", "one"), 2);
-//});
-
 //function plus(A, B) {
 //    return (0);
 //}
-//
 //it('should add "zero" and "zero"', function(){
 //    assert.equal(plus("zero", "zero"), 0);
-//});
-
-//function plus(A, B) {
-//    return (3);
-//}
-//
-//it('should add "one" and "two"', function(){
-//    assert.equal(plus("one", "two"), 3);
 //});
 
 //
 //function plus(A, B) {
 //    return (1);
 //}
-//
 //it('should add "one and "zero"', function() {
 //    assert.equal(plus("one", "zero"), 1);
 //});
+
 //
+//function plus(A, B) {
+//    return (1);
+//}
+//it('should add "zero" and "one"', function() {
+//    assert.equal(plus("zero", "one"), 1);
+//});
 
+//function plus(A, B) {
+//     return (2);
+//}
+//it('should add "one" and "one"', function(){
+//    assert.equal(plus("one", "one"), 2);
+//});
 
+//function plus(A, B) {
+//    return (3);
+//}
+//it('should add "one" and "two"', function(){
+//    assert.equal(plus("one", "two"), 3);
+//});
+
+//function plus(A, B) {
+//    return (4);
+//}
+//it('should add "one" and "three"', function() {
+//    assert.equal(plus("one", "three"), 4);
+//});
+
+//function plus(A, B) {
+//    return (5);
+//}
+//it('should add "one" and "four"', function() {
+//    assert.equal(plus("one", "four"), 5);
+//});
+//
+//function plus(A, B) {
+//    return (6);
+//}
+//it('should add "one" and "five"', function() {
+//    assert.equal(plus("one", "five"), 6);
+//});
+//
+//function plus(A, B) {
+//    return (7);
+//}
+//it('should add "one" and "six"', function() {
+//    assert.equal(plus("one", "six"), 7);
+//});
+//function plus(A, B) {
+//    return (8);
+//}
+//it ('should add "one" and "seven"', function(){
+//    assert.equal(plus("one", "seven"), 8);
+//});
+//function plus(A, B) {
+//    return (9);
+//}
+//it('should add "one" and "eight"', function() {
+//   assert.equal(plus("one", "eight"), 9);
+//});
+//function plus(A, B) {
+//    return (10);
+//}
+//it('should add "one" and "nine"', function() {
+//   assert.equal(plus("one", "nine"), 10);
+//});
 function plus(A, B) {
-    return (4);
+    return (11);
 }
-it('should add "one" and "three"', function() {
-    assert.equal(plus("one", "three"), 4);
+it('should add "one" and "ten"', function() {
+    assert.equal(plus("one", "ten"), 11);
 });
