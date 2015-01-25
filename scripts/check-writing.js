145ce63f46f6e9b9e2ebaf4f7efe851b373e9d73
Jared Stevick <jstevick@gmail.com>
2015-01-25 15:19:08 -0500

    finished all ten check-writing

--- scripts/check-writing.js
+++ scripts/check-writing.js
@@ -63,5 +63,68 @@ it('should return 1 as string "one"', function() {
 //it('should return "five" when 5 is entered and "not the number 5" if any other number is entered', function() {
 //    assert.equal(toEnglish(5), "five");
 //})
+//
+//function toEnglish (a) {
+//    if (a == 6) {
+//        return "six";
+//    } else {
+//        return "this is not the number 6";
+//    }
+//}
+//console.log(toEnglish(6));
+//
+//it('should return "six" when 6 is entered and "not the number 6" if any other number is entered', function() {
+//    assert.equal(toEnglish(6), "six");
+//})
+//
+//function toEnglish (a) {
+//    if (a == 7) {
+//        return "seven";
+//    } else {
+//        return "this is not the number 7";
+//    }
+//}
+//console.log(toEnglish(7));
+//
+//it('should return "six" when 7 is entered and "not the number 7" if any other number is entered', function() {
+//    assert.equal(toEnglish(7), "seven");
+//})
+//
+//function toEnglish (a) {
+//    if (a == 8) {
+//        return "eight";
+//    } else {
+//        return "this is not the number 8";
+//    }
+//}
+//console.log(toEnglish(8));
+//
+//it('should return "eight" when 8 is entered and "not the number 8" if any other number is entered', function() {
+//    assert.equal(toEnglish(8), "eight");
+//})
+//function toEnglish (a) {
+//    if (a == 9) {
+//        return "nine";
+//    } else {
+//        return "this is not the number 9";
+//    }
+//}
+//console.log(toEnglish(9));
+//
+//it('should return "nine" when 9 is entered and "not the number 9" if any other number is entered', function() {
+//    assert.equal(toEnglish(9), "nine");
+//})
 
 
+function toEnglish (a) {
+    if (a == 10) {
+        return "ten";
+    } else {
+        return "this is not the number ten";
+    }
+}
+console.log(toEnglish(6));
+
+it('should return "ten" when 10 is entered and "not the number 10" if any other number is entered', function() {
+    assert.equal(toEnglish(10), "ten");
+})
