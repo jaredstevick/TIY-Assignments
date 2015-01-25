287fa6fce3ccac14ec83a79e96a1380da78a5945
Jared Stevick <jstevick@gmail.com>
2015-01-25 15:07:42 -0500

    string calculator 1-5

---
+++ scripts/check-writing.js
@@ -0,0 +1,67 @@
+var assert = require('assert');
+
+/*function toEnglish (a) {
+    return "one";
+}
+it('should return 1 as string "one"', function() {
+    assert.equal(toEnglish(1), "one");
+})*/
+
+//function toEnglish(a) {
+//    if (a == 2) {
+//        return "two";
+//    }
+//
+//    else {
+//        return "this is not the number 2";
+//    }
+//}
+//
+//console.log(toEnglish(2));
+//
+//it('should return "two" when 2 is entered and "not two" if any other number is entered', function () {
+//    assert.equal(toEnglish(2), "two");
+//
+//} )
+
+//function toEnglish (a) {
+//    if (a == 3) {
+//        return "three";
+//    } else {
+//        return "this is not the number 3";
+//    }
+//}
+//console.log(toEnglish(3));
+//
+//it('should return "three" when 3 is entered and "not the number 3" if any other number is entered', function() {
+//    assert.equal(toEnglish(3), "three");
+//
+//})
+
+//function toEnglish (a) {
+//    if (a == 4) {
+//    return "four";
+//    } else {
+//        return "this is not the number 4";
+//    }
+//}
+//console.log(toEnglish(4));
+//it('should return "four" when 4 is entered and "not the number 4" if any other number is entered', function() {
+//    assert.equal(toEnglish(4), "four");
+//})
+//
+//
+//function toEnglish (a) {
+//    if (a == 5){
+//        return "five";
+//    } else {
+//        return "this is not the number 5";
+//    }
+// }
+//console.log(toEnglish(5));
+//
+//it('should return "five" when 5 is entered and "not the number 5" if any other number is entered', function() {
+//    assert.equal(toEnglish(5), "five");
+//})
+
+
