b399b4189510db1ccbf7b82551edb63b6e38cbf5
Jared Stevick <jstevick@gmail.com>
2015-01-28 18:21:25 -0500

    beginning to rewrite chessboard with js

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,14 @@
-var board = [
-    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
-    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
-    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] ];
-
-console.log(board.join('\n') + '\n\n');
+//var board = [
+//    ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'],
+//    ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+//    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+//    ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+//    ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'] ];
+//
+//console.log(board.join('\n') + '\n\n');
 
 // Move King's Pawn forward 2
 //board[4][4] = board[6][4];
@@ -28,24 +28,55 @@ console.log(board.join('\n') + '\n\n');
 // success!!
 
 //to print the board
-var printBoard = function() {
-    console.log(board.join('\n'));
-}
+//var printBoard = function() {
+//    console.log(board.join('\n'));
+//}
+
+//trying out the new board made with the javascripts
+var _ = require('lodash')
+function print(board){
+  // Before any rows are printed, print the `<table>`...
+  console.log('<table class="chessboard">');
+
+  _.forEach(board, function(row, rank){
+
+      // Before any cells are printed...
+
+      _.forEach(row, function(square, file){
+
+          // In here, we have `rank`, `file`, and `square`...
+
+      }); // END foreach(row)
+
+      // After all the cells are printed...
+
+  }); // END foreach(board)
+
+  // After all the rows are printed...
+  console.log('</table> <!-- .chessboard -->');
+} // END print
+
+
+
+
+
+
+
 
 
 //let's move some pieces
-var movePiece = function (fromX, fromY, toX, toY) {
-// to make it match our board numbers and avoid confusion, subtract one(since arrays start at 0)
-    fromX = fromX - 1;
-    fromY = fromY - 1;
-    toX = toX - 1;
-    toY = toY - 1;
-//the actual movement of the piece
-    board[toY][toX] = board[fromY][fromX];
-    board[fromY][fromX] = ' ';
-//don't forget to redraw the board
-    printBoard();
-};
+//var movePiece = function (fromX, fromY, toX, toY) {
+//// to make it match our board numbers and avoid confusion, subtract one(since arrays start at 0)
+//    fromX = fromX - 1;
+//    fromY = fromY - 1;
+//    toX = toX - 1;
+//    toY = toY - 1;
+////the actual movement of the piece
+//    board[toY][toX] = board[fromY][fromX];
+//    board[fromY][fromX] = ' ';
+////don't forget to redraw the board
+//    printBoard();
+//};
 
 //testing testing
 //movePiece(4, 6, 4, 5);
