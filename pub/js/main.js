003bb5d40486dce353524b6b784deec4b5c19f60
Jared Stevick <jstevick@gmail.com>
2015-02-03 00:55:40 -0500

    refactored and added js. only first one hides

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,17 @@
 
+(function () {
+
+    var squeezebox = document.querySelector('.squeezebox_toggle');
+    var toggleState = function (elem, one, two) {
+        var elem = document.querySelector(elem);
+        elem.setAttribute('data-state', elem.getAttribute('data-state') === one ? two : one);
+    };
+
+    squeezebox.onclick = function (e) {
+        toggleState('p', 'closed', 'open');
+        e.preventDefault();
+
+
+    };
+
+})();
