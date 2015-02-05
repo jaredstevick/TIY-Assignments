148af6fd4943cf7af6656c1b6e64f38ac9be7dc5
Jared Stevick <jstevick@gmail.com>
2015-02-05 02:10:02 -0500

    tiny part of an mf'ing website. must sleep

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,15 @@
 
+$('#about a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#repos a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
+
+$('#public a').click(function (e) {
+  e.preventDefault()
+  $(this).tab('show')
+})
