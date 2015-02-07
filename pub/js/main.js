8be50bbb16e2caf292e19dd1dd553f72a34754e5
Jared Stevick <jstevick@gmail.com>
2015-02-07 18:40:57 -0500

    tried to simplify. now i can write my name with json and jquery. YESsss

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,11 +1,9 @@
-(function (window) {
-    'use strict';
 
 
 $(document).ready(function() {
 
-$.getJSON('..apis/github/users/jaredstevick/jaredstevick.json', function(json) {
-    console.log(json.login);
+$.getJSON('jaredstevick.json', function(json) {
+    console.log(json.login); //testing
 });
 
 
@@ -14,12 +12,15 @@ $('button').click(function makewords() {
 $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/255/comments?access_token=de175947bb3069ad32ac382439d7f929609c5901", JSON.stringify({"body": $("#textbox").val()}), $("#textbox").val(""));
 });
 
+$.get("jaredstevick.json", function(user) {
+    $('aside').find('h2').append(user.name);}, "json");
+
 
 
 
 
 });
-})(window);
+
 //example
 //$("h1").text("changed");
 
