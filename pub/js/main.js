a5ac3555d0d311ec225afe09ebf1f85610715b96
Jared Stevick <jstevick@gmail.com>
2015-02-07 21:04:45 -0500

    refactored..very nice

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
 
 $.getJSON('jaredstevick.json', function(json) {
@@ -7,25 +5,22 @@ $.getJSON('jaredstevick.json', function(json) {
 });
 
 
+
+
+
+$.get("jaredstevick.json", function(user) {
+    $('aside').find('h2').append(user.name),
+    $('aside').find('h3').append(user.login),
+    $('aside').find('a').append(user.email),
+    $('aside').find('.location').append(user.location),
+    $('aside').find('.joined').append(user.created_at);}, "json");
+
+
+
 $('button').click(function makewords() {
     //function
 $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/255/comments?access_token=de175947bb3069ad32ac382439d7f929609c5901", JSON.stringify({"body": $("#textbox").val()}), $("#textbox").val(""));
 });
-
-$.get("jaredstevick.json", function(user) {
-    $('aside').find('h2').append(user.name);}, "json");
-
-$.get("jaredstevick.json", function(user) {
-    $('aside').find('h3').append(user.login);}, "json");
-
-$.get("jaredstevick.json", function(user) {
-    $('aside').find('a').append(user.email);}, "json");
-
-$.get("jaredstevick.json", function(user) {
-    $('aside').find('.location').append(user.location);}, "json");
-
-$.get("jaredstevick.json", function(user) {
-    $('aside').find('.joined').append(user.created_at);}, "json");
 
 });
 
