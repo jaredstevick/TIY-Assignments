abf85a2956c998a9577337849898f4a41f760f26
Jared Stevick <jstevick@gmail.com>
2015-02-09 16:51:23 -0500

    i can see colors lol

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,12 +1,29 @@
 //where do i even start...
 //maybe with just displaying the time
-
+//lets display the current time
+var d, h, m, s, colorTime;
+//sorry moment.js, maybe next time
 
 function showTime() {
+    d = new Date();
+    h = d.getHours();
+    m = d.getMinutes();
+    s = d.getSeconds();
 
-document.getElementById("clock").innerHTML = Date();
-setInterval(showTime, 1000);
+//  need more zero to keep six digits!
+    if(h <= 9) h = '0' + h;
+    if(m <= 9) m = '0' + m;
+    if(s <= 9) s = '0' + s;
 
+    colorTime = "#" + h + m + s;
+
+    document.body.style.background = colorTime;
+
+    document.getElementById("clock").innerHTML = colorTime;
+
+
+    setInterval(showTime, 1000);
 }
+
 
 showTime();
