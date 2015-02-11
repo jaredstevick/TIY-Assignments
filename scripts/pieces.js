1997da7202f0ce97966d1a5f16e25d0b35fc0a03
Jared Stevick <jstevick@gmail.com>
2015-02-11 01:44:54 -0500

    more tests

--- scripts/pieces.js
+++ scripts/pieces.js
@@ -25,7 +25,7 @@ Piece.prototype = {
         return this._name; //returns the name of the piece
     },
     abbrev: function() {
-        return name.substring(0,1); //takes the first letter of the string 'name'
+        return this._name.substring(0,2); //takes the first letter of the string 'name'. one letter isn't enough if i spell knight right
     },
     toString: function() {
         return this._color + " " + this._name + " " + this._position; //returns the parameters of piece as a string
@@ -49,13 +49,35 @@ console.log(King.position());
 var Queen = new Piece('black', 'Queen', 'd8');
 console.log(Queen.name());
 console.log(Queen.color());
+console.log(Queen.position());
+
+var Bishop = new Piece('black', 'Bishop', 'c8');
+Bishop.queenside = true;
+console.log(Bishop.name());
+console.log(Bishop.color());
+console.log(Bishop.position());
+console.log(Bishop.queenside);
 
 
+var Knight = new Piece('black', 'Knight', 'b8');
+console.log(Knight.name());
+console.log(Knight.color());
+console.log(Knight.position());
 
+var Rook = new Piece('white', 'Rook', 'a1');
+console.log(Rook.name());
+console.log(Rook.color());
+console.log(Rook.position());
 
+var Pawn = new Piece('white', 'Pawn', 'a2');
+console.log(Pawn.name());
+console.log(Pawn.color());
+console.log(Pawn.position());
 
-
-
+var Godzilla = new Piece('green', 'Dinosaur', 'd5');
+console.log(Godzilla.name());
+console.log(Godzilla.color());
+console.log(Godzilla.position());
 //function Rook(color, queenside) {
 //    Piece.call(this, color, 'rook', position);
 //}
@@ -97,49 +119,43 @@ it("should test if king is black", function() {
 });
 
 it("should test if king is purple", function() {
-    assert(King.color == "purple");
+    assert(King.color() == "purple");
 });
-//
-//var Pawn = new Piece ('white', 'Pawn', 'C2');
-//var Queen = new Piece ('white', 'Queen', 'D1' );
-//var Knight = new Piece('white','Knight', 'E1');
-//Knight.queenside = "true";
+
+it("should test if Knight is green", function() {
+    assert(Knight.color() == "green");
+});
+
+it("should test if Knight is black", function() {
+    assert(Knight.color() == "black");
+});
+
+it('should test if Knight is in position b8', function() {
+    assert(Knight.position() == "b8");
+});
+it("should test if the abbrev for knight is kn", function() {
+    assert(Knight.abbrev == 'Kn');
+});//why doesn't this one work?
+
+it("should test if Rook is white", function() {
+    assert(Rook.color() =="white");
+});
+
+it("should test if Rook is a rook", function() {
+    assert(Rook.name() =="Rook");
+});
+
+it("should test if pawn is white", function() {
+    assert(Pawn.color() =="white");
+});
+
+it("should test if Pawn is a pawn", function() {
+    assert(Pawn.name() =="Pawn");
+});
+
+it("should test if Godzilla is green", function() {
+    assert(Godzilla.color() == "green");
+});
 
 
 
-
-//content goes here
-/**
- * @constructor Piece
- * @param String color of Piece: "white" or "black"
- * @param String name of Piece, e.g. "queen", "rook", "knight"
- * @param String position of Piece, e.g. "a7", "d8"
- *
- * @method position
- * @returns String current position, e.g. "a8", "f3"
- *
- * @method color
- * @returns String color of Piece: "white" or "black"
- *
- * @method name
- * @returns String name of Piece, e.g. "queen", "rook", "knight"
- *
- * @method abbrev
- * @returns String abbreviation of Piece, e.g. "white queen" == 'q'
- *
- * @method toString()
- * @return String for printing / debugging, e.g. "black queen d8"
- *
- * @method toHTML()
- * @return String HTML entity, e.g. "black knight" == "&#x265e"
- *
- * @method toUTF8()
- * @return String UTF8 character, e.g. "black knight" == "\u265e"
- */
-//function Piece(color, name, position){
-  // Constructor
-//}
-//
-//Piece.prototype = {
-//}
-// END Piece.prototype
