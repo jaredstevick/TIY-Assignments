e6b47d08eceaac40d624c7169c7995f17db01917
Jared Stevick <jstevick@gmail.com>
2015-02-11 01:05:16 -0500

    making some progress with tests and slowly building some pieces maybe

--- scripts/pieces.js
+++ scripts/pieces.js
@@ -9,28 +9,52 @@ function pieceMaker() {
 
 function Piece(color, name, position) {
     //attach properties here
-    this.color = color;
-    this.name = name;
-    this.position = position;
+    this._color = color;
+    this._name = name;
+    this._position = position;
 }
 
 Piece.prototype = {
     position: function() {
-        return this.position; //return the position of the piece
+        return this._position; //return the position of the piece
     },
     color: function() {
-        return this.color; //return the color of the piece
+        return this._color; //return the color of the piece
     },
     name: function() {
-        return this.name; //returns the name of the piece
+        return this._name; //returns the name of the piece
     },
     abbrev: function() {
         return name.substring(0,1); //takes the first letter of the string 'name'
     },
     toString: function() {
-        return this.color + " " + this.name + " " + this.position; //returns the parameters of piece as a string
+        return this._color + " " + this._name + " " + this._position; //returns the parameters of piece as a string
+    },
+    toHTML: function() {
+        return
+    },
+    toUTF: function() {
+
     }
  };
+
+
+
+
+var King = new Piece('black', 'King', 'e8');
+console.log(King.name());
+console.log(King.color());
+console.log(King.position());
+
+var Queen = new Piece('black', 'Queen', 'd8');
+console.log(Queen.name());
+console.log(Queen.color());
+
+
+
+
+
+
 
 //function Rook(color, queenside) {
 //    Piece.call(this, color, 'rook', position);
@@ -38,15 +62,48 @@ Piece.prototype = {
 //
 //Rook.prototype = new Piece();
 
-it("should have a function called piece", function() {
+it("should have a piece", function() {
     assert(Piece);
 });
 
 
 it("should test if piece is a function", function() {
-    assert(typeof Piece == "object");
+    assert(typeof Piece == "function");
 });
 
+
+it("should test if piece has a position", function() {
+    assert(Piece.position == "e8");
+});
+
+it("should test if king has a color", function() {
+   assert(King.color() == "color");
+   });
+
+it("should test if queen is a pawn", function() {
+   assert(Queen.name() == "pawn");
+   });
+
+it("should test if queen is a queen", function() {
+    assert(Queen.name() == "Queen");
+});
+
+it('should test if queen is in position d8', function() {
+    assert(Queen.position() == "d8");
+});
+
+it("should test if king is black", function() {
+    assert(King.color() == "black");
+});
+
+it("should test if king is purple", function() {
+    assert(King.color == "purple");
+});
+//
+//var Pawn = new Piece ('white', 'Pawn', 'C2');
+//var Queen = new Piece ('white', 'Queen', 'D1' );
+//var Knight = new Piece('white','Knight', 'E1');
+//Knight.queenside = "true";
 
 
 
