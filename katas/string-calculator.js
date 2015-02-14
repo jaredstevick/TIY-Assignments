21800fbe9f9156f56bdd088b0f345af9610611e6
Jared Stevick <jstevick@gmail.com>
2015-02-14 00:45:05 -0500

    added numbers 11-20

--- katas/string-calculator.js
+++ katas/string-calculator.js
@@ -12,9 +12,20 @@ function stringCalc(A, B){
 		"seven": 7,
 		"eight": 8,
 		"nine": 9,
-		"ten": 10
+		"ten": 10,
+		"eleven": 11,
+		"twelve": 12,
+		"thirteen": 13,
+		"fourteen": 14,
+		"fifteen": 15,
+		"sixteen": 16,
+		"seventeen": 17,
+		"eightteen": 18,
+		"nineteen": 19,
+		"twenty": 20
 	};
-	return numbers[A] + numbers[B];
+	
+	return numbers [A] + numbers[B];
 };
 
 it('should add one and purple', function(){
@@ -29,5 +40,10 @@ it('should add three and four', function(){
 	assert.equal(stringCalc("three", "four"), 7);
 });
 
+it('should add eleven and nine', function(){
+	assert.equal(stringCalc("eleven", "nine"), 20);
+});
+
 console.log(stringCalc("three", "two"));
 console.log(stringCalc("seven", "one"));
+console.log(stringCalc("four" + "three"));
