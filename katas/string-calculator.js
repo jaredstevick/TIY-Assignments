918ed4741ea90afae26b7ad1b29211ad1337a72b
Jared Stevick <jstevick@gmail.com>
2015-02-14 18:47:17 -0500

    added numbers up to 100

--- katas/string-calculator.js
+++ katas/string-calculator.js
@@ -21,7 +21,14 @@ var numbers = {
 	"seventeen": 17,
 	"eightteen": 18,
 	"nineteen": 19,
-	"twenty": 20
+	"twenty": 20,
+	"thirty": 30,
+	"forty": 40,
+	"fifty": 50,
+	"sixty": 60,
+	"seventy": 70,
+	"eighty": 80,
+	"ninety": 90
 };
 
 function plus(A, B){
@@ -98,7 +105,7 @@ it('should return the result of nineteen * seventeen * twenty', function(){
 console.log(toNum("nineteen") * toNum("seventeen") * toNum("twenty"));
 
 
-//this is kinda hacky i think, but it totally works
+//this is kinda hacky i think, but it totally works.  just grabbing and chopping up a string
 console.log(("twentytwo").substring(0, 6));
 
 function moreNums(A){
@@ -108,6 +115,25 @@ function moreNums(A){
 	if (A.substring(0, 6) === "thirty") {
 		return 30 + toNum(A.substring(6, 11));
 	}
+	if (A.substring(0, 5) === "forty") {
+		return 40 + toNum(A.substring(5, 11));
+	};
+	if (A.substring(0, 5) === "fifty") {
+		return 50 + toNum(A.substring(5, 11));
+	};
+	if (A.substring(0, 5) === "sixty") {
+		return 60 + toNum(A.substring(5, 11));
+	};
+	if (A.substring(0, 7) === "seventy") {
+		return 70 + toNum(A.substring(7, 12));
+	};
+	if (A.substring(0, 6) === "eighty") {
+		return 80 + toNum(A.substring(6, 11));
+	};
+	if (A.substring(0, 6) === "ninety") {
+		return 90 + toNum(A.substring(6, 11));
+	};
+
 };
 
 it('should return 29 for twentynine', function(){
@@ -119,3 +145,28 @@ it('should return 34 for thirtyfour', function(){
 	assert.equal(moreNums("thirtyfour"), 34);
 });
 console.log(moreNums("thirtyfour"));
+
+it('should return 41 for fortyone', function(){
+	assert.equal(moreNums("fortyone"), 41);
+});	
+console.log(moreNums("fortyone"));
+
+it('should return 53 for fiftythree', function(){
+	assert.equal(moreNums("fiftythree"), 53);
+});	
+console.log(moreNums("fiftythree"));
+
+it('should return 64 for sixtyfour', function(){
+	assert.equal(moreNums("sixtyfour"), 64);
+});	
+console.log(moreNums("sixtyfour"));
+
+it('should return 78 for seventyeight', function(){
+	assert.equal(moreNums("seventyeight"), 78);
+});	
+console.log(moreNums("seventyeight"));
+
+it('should return 95 for ninetyfive', function(){
+	assert.equal(moreNums("ninetyfive"), 95);
+});	
+console.log(moreNums("ninetyfive"));
