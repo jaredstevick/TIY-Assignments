361974b92bbeb1a8b39a8800ece8ad665c7ad521
Jared Stevick <jstevick@gmail.com>
2015-02-14 21:53:40 -0500

    added a few more requests and wrote a couple of tests

--- apis/gymnastics.js
+++ apis/gymnastics.js
@@ -14,22 +14,49 @@ var issueComment = events.filter(function(item){
         return item.type == 'IssueCommentEvent';
     });
 
-/*
-function answer(){
-  return {
-    'total': ...,    // How many total events did you fetch?
-    'PushEvent': {
-      'total': ...,  // How many total events of type `PushEvent` are there?
-      'perDay': ...  // On average, how many`PushEvent` entries per day?
-    },
-    'other': {
-      'total': ...,  // How many _other_ events are in the data?
-      'perDay': ...  // How many per day, on average?
-    }
-  };
-} // END answer */
+var createEvent = events.filter(function(item){
+	return item.type == 'CreateEvent'
+});
+
+var otherEvents = events.filter(function(item){
+	return item.type != 'PushEvent'
+});
+
+var deleteEvent = events.filter(function(item){
+	return item.type == 'DeleteEvent'
+});
+
+var otherEvents = events.filter(function(item){
+	return item.type != 'PushEvent'
+});
+
+
+// function theAnswer(){
+//   return {
+//     'total': events.length,
+//     'PushEvent': {
+//       'total': pushEvents.length,
+//       'perDay': ...  // On average, how many`PushEvent` entries per day?
+//     },
+//     'other': {
+//       'total': ...,  // How many _other_ events are in the data?
+//       'perDay': ...  // How many per day, on average?
+//     }
+//   };
+// } // END answer 
+
+it('should have 30 total events', function() {
+    assert(events.length === 30);
+    });
+
+it('should have 14 `PushEvent` entries', function(){
+    assert(pushEvents.length === 14);
+    });
 
 console.log(events.length);
 console.log(pushEvents.length);
 console.log(pullRequests.length);
 console.log(issueComment.length);
+console.log(createEvent.length);
+console.log(deleteEvent.length);
+console.log(otherEvents.length);
