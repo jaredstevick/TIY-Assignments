b9c86a38713975042ec3f9dba4e4095d929a3807
Jared Stevick <jstevick@gmail.com>
2015-02-14 19:26:02 -0500

    checkwriting 1-20

--- katas/check-writing.js
+++ katas/check-writing.js
@@ -1,3 +1,38 @@
-function tocheck(n){
+var assert = require('assert');
 
+var numbers = [ 
+	"zero",
+	"one", 
+	"two", 
+	"three", 
+	"four", 
+	"five", 
+	"six", 
+	"seven", 
+	"eight", 
+	"nine", 
+	"ten", 
+	"eleven", 
+	"twelve", 
+	"thirteen", 
+	"fourteen", 
+	"fifteen", 
+	"sixteen", 
+	"seventeen", 
+	"eighteen", 
+	"nineteen", 
+	"twenty" ]
+
+function tocheck(n){
+	return numbers[n]
 };
+
+it('should return eleven for 11', function(){
+	assert.equal(tocheck(11), "eleven");
+});
+console.log(tocheck(11));
+
+it('should return nineteen for 19', function(){
+	assert.equal(tocheck(19), "nineteen");
+});
+console.log(tocheck(19));
