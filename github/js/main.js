39e5c80c8bd4fb551563e465fad8ef72c2a7ccde
Jared Stevick <jstevick@gmail.com>
2015-02-15 20:08:07 -0500

    created soe json files, started pulling some data, populating fields

--- github/js/main.js
+++ github/js/main.js
@@ -1,1 +1,32 @@
+$(document).ready(function() {
+
+$.getJSON("jaredstevick.json", function(user) {
+    $('aside').find('h2').append(user.name),
+    $('aside').find('h3').append(user.login),
+    $('aside').find('a').append(user.email),
+    $('aside').find('.location').append(user.location),
+    $('aside').find('.joined').append(user.created_at);
+    $('aside').find('.followers').prepend(user.followers);
+    $('aside').find('.starred').prepend(user.something);
+    $('aside').find('.following').prepend(user.following);
+});
+
+$.getJSON("repos.json", function (repoArray) {
+    $.each(repoArray, function (index, item) {
+    var repolist = $('.repos-' + index);
+    repolist.find('a').append(item.name);
+    repolist.find('p').append(item.description);
+    repolist.find('span').append(item.updated_at);
+
+
+
+    });
+});
+
+$('button').click(function makewords() {
+$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/299/comments?access_token=de175947bb3069ad32ac382439d7f929609c5901", JSON.stringify({"body": $("#textbox").val()}), $("#textbox").val(""));
+});
+
+});
+
 
