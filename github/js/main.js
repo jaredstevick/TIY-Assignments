5faa3c10b4095cdbe7838ebd42a461b8ded83097
Jared Stevick <jstevick@gmail.com>
2015-02-15 23:59:34 -0500

    finally figured out why my panels looked like crap

--- github/js/main.js
+++ github/js/main.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+var events = 'events.json';
+
+var pushEvents = (function(item){
+    return item.type == 'PushEvent'
+});
+
 $.getJSON("jaredstevick.json", function(user) {
     $('aside').find('h2').append(user.name),
     $('aside').find('h3').append(user.login),
@@ -17,11 +23,13 @@ $.getJSON("repos.json", function (repoArray) {
     repolist.find('a').append(item.name);
     repolist.find('p').append(item.description);
     repolist.find('span').append(item.updated_at);
-
-
-
     });
 });
+
+
+
+console.log(events.length);
+console.log(pushEvents.length);
 
 $('button').click(function makewords() {
 $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/299/comments?access_token=de175947bb3069ad32ac382439d7f929609c5901", JSON.stringify({"body": $("#textbox").val()}), $("#textbox").val(""));
