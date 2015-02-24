4a85c4592c4f4414bf245fd440a9033a47fc44e9
Jared Stevick <jstevick@gmail.com>
2015-02-24 16:23:57 -0500

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,6 @@ gulp.task('default', function () {
 //code goes here
 console.log('hello world!');
 });
- gulp.task('hello:watch', function(){
- gulp.watch('index.html', ['hello']);
+gulp.task('hello:watch', function(){
+gulp.watch('index.html', ['hello']);
 });  
