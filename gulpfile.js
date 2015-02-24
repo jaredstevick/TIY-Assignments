899b6c9e8de7c1d47e59a666011b7b982d9a4ed2
Jared Stevick <jstevick@gmail.com>
2015-02-24 16:26:06 -0500

    updating gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+ var gulp = require('gulp')
+gulp.task('default', function () {
+//code goes here
+console.log('hello world!');
+});
+gulp.task('hello:watch', function(){
+gulp.watch('index.html', ['hello']);
+});  
